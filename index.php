bd3588532c7845369ae1d089c9ba67d42e6bbad1
questaoresponse <eneagonlosamigos@gmail.com>
2023-10-13 00:43:33 -0300

    adicionar pasta

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "eae";
-$url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+//$url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+$url=$_GET['route'];
 echo $url;
 // Roteamento com base na URL
 switch ($url) {
@@ -22,26 +23,26 @@ switch ($url) {
         require __DIR__ . '/templates/index.php';
         break;
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($url=="/admin"){
-    // Verifica se a solicitação é do tipo POST
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if ($url=="/admin"){
+//     // Verifica se a solicitação é do tipo POST
 
-    // Recupere os dados do formulário usando a variável $_POST
-    $email = $_POST["email"];
-    $senha = $_POST["password"];
+//     // Recupere os dados do formulário usando a variável $_POST
+//     $email = $_POST["email"];
+//     $senha = $_POST["password"];
 
-    // Faça o que desejar com os dados, como salvá-los em um banco de dados ou exibi-los
-    if ($email=="t@gmail.com" && $senha=="1"){
-        require __DIR__ . '/templates/admin.php' ;
-    }else{
-        require __DIR__ . '/templates/admin-error.html'
-    }
-} else {
-    // Se a solicitação não for do tipo POST, você pode exibir um formulário HTML
-    // para que os usuários possam enviar dados por POST
-    echo "Método incorreto. Use o método POST para enviar dados.";
-}
-}else{
-    echo "teste falido";
-}
+//     // Faça o que desejar com os dados, como salvá-los em um banco de dados ou exibi-los
+//     if ($email=="t@gmail.com" && $senha=="1"){
+//         require __DIR__ . '/templates/admin.php' ;
+//     }else{
+//         require __DIR__ . '/templates/admin-error.html'
+//     }
+// } else {
+//     // Se a solicitação não for do tipo POST, você pode exibir um formulário HTML
+//     // para que os usuários possam enviar dados por POST
+//     echo "Método incorreto. Use o método POST para enviar dados.";
+// }
+// }else{
+//     echo "teste falido";
+// }
 ?>
