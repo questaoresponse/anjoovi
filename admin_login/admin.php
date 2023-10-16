61d177655bc50875622256ee6766f122e5788fa7
questaoresponse <eneagonlosamigos@gmail.com>
2023-10-16 17:20:04 -0300

    adicionar pasta

--- admin_login/admin.php
+++ admin_login/admin.php
@@ -1,7 +1,7 @@
 <script>
-window.r=<?php echo json_encode($r ? $r : ["type"=>"null"]) ?>
+window.r=<?php echo json_encode($r ? $r : ["type"=>"null"]) ?>;
 </script>
 <?php
 include(__DIR__ . "/admin.html");
-include(__DIR__ . "/../admin_barra/cadastro_usuario.html");
+include("/admin_barra/cadastro_usuario.html");
 ?>
