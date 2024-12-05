14a4bb8d53b971becedce5184df99c7ae4292f82
questaoresponse <eneagonlosamigos@gmail.com>
2024-12-05 18:02:45 -0300

    update

--- dev/script.php
+++ dev/script.php
@@ -69,7 +69,7 @@ if ($emailContent) {
             "restoreValue"=>1
         ];
         $queryString=http_build_query($params);
-        $mail->Body    = "<div>https://www.anjoovi.com</div>";
+        $mail->Body    = "<div></div>";
         $mail->AltBody = "Clicke aqui.";
     }
 }
