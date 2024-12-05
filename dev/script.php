41dd4152218705f98616d759c8c7528fcbe695e4
questaoresponse <eneagonlosamigos@gmail.com>
2024-12-05 18:46:11 -0300

    update

--- dev/script.php
+++ dev/script.php
@@ -14,14 +14,8 @@ use \Firebase\JWT\JWT;
 use \Firebase\JWT\KEY;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
 function get_token($dados){
     $chaveSecreta=$GLOBALS["jwtKey"];
-    // $configuracaoToken=[
-    //     'iss' => 'http://localhost:3000',
-    //     'iat' => time(),
-    //     'exp' => strtotime('+1 year') // Adiciona um ano ao timestamp atual
-    // ];
     $payload = [
         'iat' => time(),
         'exp' => strtotime('+1 year'), // time in the past
@@ -30,39 +24,40 @@ function get_token($dados){
     return $token;
 }
 try{
-$emailContent = file_get_contents("php://stdin");
+// $emailContent = file_get_contents("php://stdin");
 
 // Opcional: Salva o e-mail em um arquivo para depuração
 
 // Processa o e-mail
-if ($emailContent) {
-    // Divida o conteúdo em cabeçalhos e corpo
-    list($headers, $body) = explode("\r\n\r\n", $emailContent, 2);
+if (true) {
+    // // Divida o conteúdo em cabeçalhos e corpo
+    // list($headers, $body) = explode("\r\n\r\n", $emailContent, 2);
 
-    // Captura o remetente
-    preg_match('/^From: (.*)$/mi', $headers, $matches);
-    $from = isset($matches[1]) ? trim($matches[1]) : '';
+    // // Captura o remetente
+    // preg_match('/^From: (.*)$/mi', $headers, $matches);
+    // $from = isset($matches[1]) ? trim($matches[1]) : '';
 
-    // Captura o assunto
-    preg_match('/^Subject: (.*)$/mi', $headers, $matches);
-    $subject = isset($matches[1]) ? trim($matches[1]) : '';
+    // // Captura o assunto
+    // preg_match('/^Subject: (.*)$/mi', $headers, $matches);
+    // $subject = isset($matches[1]) ? trim($matches[1]) : '';
 
     // Exibe as informações capturadas
     // echo "De: $from\n";
     // echo "Assunto: $subject\n";
     // echo "Corpo:\n$body\n";
-    if ($subject=="Restore" || $body=="Restore"){
+    if (true){
+        $mail = new PHPMailer(true);
         $mail->isSMTP();
         $mail->Host = 'mail.anjoovi.com'; // Altere para o seu servidor SMTP
         $mail->SMTPAuth = true;
         $mail->Username = 'account@anjoovi.com'; // Seu endereço de e-mail SMTP
-        $mail->Password = $GLOBALS["payAccountKey"]; // Sua senha de e-mail SMTP
+        $mail->Password = $GLOBALS["scriptKey"]; // Sua senha de e-mail SMTP
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Habilita a criptografia TLS
         $mail->Port = 587; // Porta TCP para conexão
     
         // Remetente e destinatário
         $mail->setFrom('account@anjoovi.com', 'Anjoovi');
-        $mail->addAddress("vter493@gmail.com", "Caro usuario");
+        $mail->addAddress("eneagonlosamigos@gmail.com", "INCÉNDIO FF");
         $mail->isHTML(true);
         $mail->Subject = 'Link para ativar.';
         $d=new DateTime();
@@ -73,8 +68,9 @@ if ($emailContent) {
             "restoreValue"=>1
         ];
         $queryString=http_build_query($params);
-        $mail->Body    = "<a>https://dev.anjoovi.com/updateCode?$queryString</a>";
+        $mail->Body    = "<a href=\"https://dev.anjoovi.com/updateCode?$queryString\"=>https://dev.anjoovi.com/updateCode?$queryString</a>";
         $mail->AltBody = "Clicke aqui.";
+        $mail->send();
     }
 }
 } catch (Exception $e){
--- dev/updateCode.php
+++ dev/updateCode.php
@@ -1,8 +1,43 @@
 <?php
+$ci=__DIR__ . '/../php-jwt/src/';
+require $ci . 'JWT.php';
+require $ci . 'JWTExceptionWithPayloadInterface.php';
+require $ci . 'BeforeValidException.php';
+require $ci . 'ExpiredException.php';
+require $ci . 'SignatureInvalidException.php';
+require $ci . 'Key.php';
+require $ci . 'JWK.php';
+require $ci . 'CachedKeySet.php';
+require __DIR__ . "/../keys/keys.php";
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\KEY;
+function jwt_verify($token){
+    $chaveSecreta=$GLOBALS["jwtKey"];
+    try {
+        $decoded=JWT::decode($token, new Key($chaveSecreta, 'HS256'));
+        return $decoded;
+    } catch (Exception $e) {
+        return false;
+    }
+}
 require __DIR__ . '/../keys/keys.php';
+$valid=false;
 if (isset($_GET["key"]) && $_GET["key"]==$GLOBALS["updateKey"]){
+    $valid=true;
+} else if (isset($_GET["token"])) {
+    $content=jwt_verify($_GET["token"]);
+    if ($content){
+        $d=new DateTime($content->d);
+        $now=new DateTime($content->d);
+        $interval=$now->diff($d);
+        if ($interval->i<5 && $interval->h==0 && $interval->d==0 && $interval->m==0 && $interval->y==0){
+            $valid=true;
+        }
+    }
+}
+if ($valid){
     if ($_GET["type"]=="restore"){
-        $restoreValue=$_POST["restoreValue"];
+        $restoreValue=$_GET["restoreValue"];
         $commands=["git fetch origin","git checkout origin/main~$restoreValue ../server","git checkout origin/main~$restoreValue ../public_html"];
         foreach ($commands as $command){
             // Array para armazenar a saída
