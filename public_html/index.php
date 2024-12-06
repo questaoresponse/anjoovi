815897a59576ee4166e4ed3be8fd4628aead6112
questaoresponse <eneagonlosamigos@gmail.com>
2024-12-06 02:35:41 -0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,11 @@
 <?php
+function cm($i){
+    return strpos($_SERVER["REQUEST_URI"],$i) === 0;
+};
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    include(__DIR__ . '/../../anjoovi/routes/routes.php');
+    include(__DIR__ . '/../server/routes.php');
+} else if (cm("/noticia") || cm("/imagem") || cm("/musica") || cm("/texto") || cm("/video")){
+    include(__DIR__ . '/../server/routes.php');
 } else {
     include(__DIR__ . '/index.html');
 }
