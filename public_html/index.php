a8493dded9a1421eab9d281d351bcfefb3a449bd
eneagonlosamigos2 <eneagonlosamigos@gmail.com>
2024-12-06 21:29:51 -0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,9 @@ function cm($i){
 };
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
     include(__DIR__ . '/../server/routes.php');
+} else if ($_SERVER["REQUEST_URI"]=="/t"){
+    echo $_SERVER["HTTP_ORIGIN"];
+    echo json_encode($_COOKIE);
 } else if (cm("/noticia") || cm("/imagem") || cm("/musica") || cm("/texto") || cm("/video")){
     include(__DIR__ . '/../server/routes.php');
 } else {
