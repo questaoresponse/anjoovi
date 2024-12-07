da162f473133bca6987f2539bfa9ee97791e247a
eneagonlosamigos2 <eneagonlosamigos@gmail.com>
2024-12-06 21:36:21 -0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -7,9 +7,21 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 } else if ($_SERVER["REQUEST_URI"]=="/t"){
     echo $_SERVER["HTTP_ORIGIN"];
     echo json_encode($_COOKIE);
+    $valorVazio = "";
+    $expiracaoPassada = time() - 3600;
+    setcookie("token", $valorVazio,[ 
+        "expires"=>$expiracaoPassada,
+        "path"=>"/",
+        "domain"=>".anjoovi.com", 
+        "secure"=>true,
+        "httponly"=>true,
+        "samesite"=>"None"]);
 } else if (cm("/noticia") || cm("/imagem") || cm("/musica") || cm("/texto") || cm("/video")){
     include(__DIR__ . '/../server/routes.php');
 } else {
     include(__DIR__ . '/index.html');
 }
+echo "<script>
+localStorage.removeItem('lg');
+</script>";
 ?>
