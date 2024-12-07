8607795874bac30ba755a1016611ee7cfc5ebe0f
eneagonlosamigos2 <eneagonlosamigos@gmail.com>
2024-12-06 21:51:31 -0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     setcookie("token", $valorVazio,[ 
         "expires"=>$expiracaoPassada,
         "path"=>"/",
-        "domain"=>".anjoovi.com", 
+        "domain"=>"", 
         "secure"=>true,
         "httponly"=>true,
         "samesite"=>"None"]);
