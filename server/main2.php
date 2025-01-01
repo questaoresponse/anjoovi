c9ca94e08a1e1197ef83a2ea5bbb23a32e9f0f14
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-01 01:51:30 -0300

    "update"

--- server/main2.php
+++ server/main2.php
@@ -282,8 +282,8 @@ function getAlgoritmoNoticia($isGeral,$conn,$usuario,$id,$pt=0,$limit=48){
             return p($conn->prepare("WITH history AS (
                 SELECT 
                     h.usuario,
-                    MAX(CASE WHEN h.rnk = 1 THEN CONCAT('%', LOWER(h.texto), '%') ELSE NULL END) AS latest_text,
-                    MAX(CASE WHEN h.rnk = 2 THEN CONCAT('%', LOWER(h.texto), '%') ELSE NULL END) AS second_latest_text
+                    MAX(CASE WHEN h.rnk = 1 THEN CONCAT('%', h.texto, '%') ELSE NULL END) AS latest_text,
+                    MAX(CASE WHEN h.rnk = 2 THEN CONCAT('%', h.texto, '%') ELSE NULL END) AS second_latest_text
                 FROM (
                     SELECT 
                         usuario,
@@ -322,11 +322,11 @@ function getAlgoritmoNoticia($isGeral,$conn,$usuario,$id,$pt=0,$limit=48){
                 tipo,
                 (
                     CASE 
-                        WHEN LOWER(IFNULL(IFNULL(p.titulo,p.descricao),p.texto)) LIKE (h.latest_text) THEN 1 
+                        WHEN LOWER(IFNULL(IFNULL(p.titulo,p.descricao),p.texto)) LIKE LOWER(h.latest_text) THEN 1 
                         ELSE 0 
                     END + 
                     CASE 
-                        WHEN LOWER(IFNULL(IFNULL(p.titulo,p.descricao),p.texto)) LIKE (h.second_latest_text) THEN 0.5 
+                        WHEN LOWER(IFNULL(IFNULL(p.titulo,p.descricao),p.texto)) LIKE LOWER(h.second_latest_text) THEN 0.5 
                         ELSE 0 
                     END
                 ) AS accuracy 
