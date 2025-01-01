21aaabf6756bebfe29aca8af4b30779f93d8ea92
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-01 01:24:27 -0300

    "update"

--- server/main2.php
+++ server/main2.php
@@ -288,7 +288,7 @@ function getAlgoritmoNoticia($isGeral,$conn,$usuario,$id,$pt=0,$limit=48){
                     SELECT 
                         usuario,
                         CONCAT('%',texto,'%') AS texto,
-                        ROW_NUMBER() OVER (PARTITION BY usuario ORDER BY id DESC) AS rnk
+                        ROW_NUMBER() OVER (PARTITION BY usuario ORDER BY id) AS rnk
                     FROM historico
                 ) h
                 WHERE h.rnk <= 2
