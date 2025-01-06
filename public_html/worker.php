99aff77a3c55a6b61ed6a01bc69c31675f0f99ce
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:26:58 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -1,9 +1,9 @@
 <?php
-function cm($i){
-    return strpos($_SERVER["REQUEST_URI"],$i) === 0;
-};
-if (cm("/admin") && (cm("/admin/noticias_cadastro") || cm("/admin/24_cadastro") || cm("/admin/imagens_cadastro") || cm("/admin/musicas_cadastro") || cm("/admin/textos_cadastro") || cm("/admin/videos_cadastro") || cm("/admin/products_cadastro"))){
+// function cm($i){
+//     return strpos($_SERVER["REQUEST_URI"],$i) === 0;
+// };
+// if (cm("/admin") && (cm("/admin/noticias_cadastro") || cm("/admin/24_cadastro") || cm("/admin/imagens_cadastro") || cm("/admin/musicas_cadastro") || cm("/admin/textos_cadastro") || cm("/admin/videos_cadastro") || cm("/admin/products_cadastro"))){
     header("Cross-Origin-Opener-Policy: same-origin");
     header("Cross-Origin-Embedder-Policy: require-corp");
-}
+// }
 // include(__DIR__ . "/assets/" . array_splice(explode("/",$_SERVER["REQUEST_URI"]),-1));
