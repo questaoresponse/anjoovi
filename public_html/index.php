87425a0487075c4eb3db850f42852127a31db139
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:06:26 -0300

    "update"

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,6 @@ function cm($i){
 function cm2($s,$i){
     return strpos($s,$i) === 0;
 };
-echo implode("/",array_slice(explode("/",$_SERVER["HTTP_REFERER"]),3));
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
     include(__DIR__ . '/../server/routes.php');
 } else if (cm("/noticia") || cm("/imagem") || cm("/musica") || cm("/texto") || cm("/video") || cm("/playlist") || cm("/product")){
@@ -13,6 +12,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 } else {
     if ($_SERVER["HTTP_REFERER"]){
         $s=implode("/",array_slice(explode("/",$_SERVER["HTTP_REFERER"]),3));
+        echo cm2($s,"admin");
         if (cm2($s,"admin") && (cm2($s,"admin/noticias_cadastro") || cm2($s,"admin/24_cadastro") || cm2($s,"admin/imagens_cadastro") || cm2($s,"admin/musicas_cadastro") || cm2($s,"admin/textos_cadastro") || cm2($s,"admin/videos_cadastro") || cm2($s,"admin/products_cadastro"))){
             header("Cross-Origin-Opener-Policy: same-origin");
             header("Cross-Origin-Embedder-Policy: require-corp");
