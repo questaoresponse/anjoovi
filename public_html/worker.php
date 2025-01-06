6b9dc5015139cbe6642c7d71f1388a3b2947c754
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:33:40 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -8,5 +8,6 @@ if ($_SERVER["HTTP_REFERER"]){
         header("Cross-Origin-Opener-Policy: same-origin");
         header("Cross-Origin-Embedder-Policy: require-corp");
     }
+    echo $s;
 }
 include(__DIR__ . "/assets/" . explode("/",$_SERVER["REQUEST_URI"])[2]);
