45f84160f3ee93308827e0f736b2607321655fb2
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:31:34 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -2,9 +2,11 @@
 function cm($i){
     return strpos($_SERVER["REQUEST_URI"],$i) === 0;
 };
-// if (cm("/admin") && (cm("/admin/noticias_cadastro") || cm("/admin/24_cadastro") || cm("/admin/imagens_cadastro") || cm("/admin/musicas_cadastro") || cm("/admin/textos_cadastro") || cm("/admin/videos_cadastro") || cm("/admin/products_cadastro"))){
-    header("Cross-Origin-Opener-Policy: same-origin");
-    header("Cross-Origin-Embedder-Policy: require-corp");
-// }
-echo cm("/assets");
+if ($_SERVER["HTTP_REFERER"]){
+    $s=implode("/",array_slice(explode("/",$_SERVER["HTTP_REFERER"]),3));
+    if (cm2($s,"admin") && (cm2($s,"admin/noticias_cadastro") || cm2($s,"admin/24_cadastro") || cm2($s,"admin/imagens_cadastro") || cm2($s,"admin/musicas_cadastro") || cm2($s,"admin/textos_cadastro") || cm2($s,"admin/videos_cadastro") || cm2($s,"admin/products_cadastro"))){
+        header("Cross-Origin-Opener-Policy: same-origin");
+        header("Cross-Origin-Embedder-Policy: require-corp");
+    }
+}
 include(__DIR__ . "/assets/" . explode("/",$_SERVER["REQUEST_URI"])[2]);
