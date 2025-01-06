b67c23421e65d6b87955380fc2e9c1e02ae024e2
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:35:04 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -2,6 +2,8 @@
 function cm2($i){
     return strpos($s,$i) === 0;
 };
+echo $_SERVER["HTTP_REFERER"];
+echo "oie";
 if ($_SERVER["HTTP_REFERER"]){
     $s=implode("/",array_slice(explode("/",$_SERVER["HTTP_REFERER"]),3));
     if (cm2($s,"admin") && (cm2($s,"admin/noticias_cadastro") || cm2($s,"admin/24_cadastro") || cm2($s,"admin/imagens_cadastro") || cm2($s,"admin/musicas_cadastro") || cm2($s,"admin/textos_cadastro") || cm2($s,"admin/videos_cadastro") || cm2($s,"admin/products_cadastro"))){
