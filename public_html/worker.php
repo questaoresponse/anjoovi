6b103ff55dd6361e0b4ad9c7d30cae3b04b9633d
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:40:09 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -1,13 +1,5 @@
 <?php
-function cm2($i){
-    return strpos($s,$i) === 0;
-};
-if ($_SERVER["HTTP_REFERER"]){
-    $s=implode("/",array_slice(explode("/",$_SERVER["HTTP_REFERER"]),3));
-    if (cm2($s,"admin") && (cm2($s,"admin/noticias_cadastro") || cm2($s,"admin/24_cadastro") || cm2($s,"admin/imagens_cadastro") || cm2($s,"admin/musicas_cadastro") || cm2($s,"admin/textos_cadastro") || cm2($s,"admin/videos_cadastro") || cm2($s,"admin/products_cadastro"))){
-        header("Cross-Origin-Opener-Policy: same-origin");
-        header("Cross-Origin-Embedder-Policy: require-corp");
-    }
-}
+header("Cross-Origin-Opener-Policy: same-origin");
+header("Cross-Origin-Embedder-Policy: require-corp");
 header('Content-Type: application/javascript');
 include(__DIR__ . "/assets/" . explode("/",$_SERVER["REQUEST_URI"])[2]);
