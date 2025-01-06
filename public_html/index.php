c8dfbac86572e62b945eb480cd85fc1de42da5ba
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:00:19 -0300

    "update"

--- public_html/index.php
+++ public_html/index.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     include(__DIR__ . '/../server/routes.php');
 } else {
     if ($_SERVER["HTTP_REFERER"]){
-        $s=exlode("/",$_SERVER["HTTP_REFERER"])[2];
+        $s=exlode("/",$_SERVER["HTTP_REFERER"])[3];
         if (cm2($s,"admin") && (cm2($s,"admin/noticias_cadastro") || cm2($s,"admin/24_cadastro") || cm2($s,"admin/imagens_cadastro") || cm2($s,"admin/musicas_cadastro") || cm2($s,"admin/textos_cadastro") || cm2($s,"admin/videos_cadastro") || cm2($s,"admin/products_cadastro"))){
             header("Cross-Origin-Opener-Policy: same-origin");
             header("Cross-Origin-Embedder-Policy: require-corp");
