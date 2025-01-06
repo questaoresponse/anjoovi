9fc8b61f0a4252deed71d04161178696930401d4
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-05 23:30:39 -0300

    "update"

--- public_html/worker.php
+++ public_html/worker.php
@@ -1,9 +1,10 @@
 <?php
-// function cm($i){
-//     return strpos($_SERVER["REQUEST_URI"],$i) === 0;
-// };
+function cm($i){
+    return strpos($_SERVER["REQUEST_URI"],$i) === 0;
+};
 // if (cm("/admin") && (cm("/admin/noticias_cadastro") || cm("/admin/24_cadastro") || cm("/admin/imagens_cadastro") || cm("/admin/musicas_cadastro") || cm("/admin/textos_cadastro") || cm("/admin/videos_cadastro") || cm("/admin/products_cadastro"))){
     header("Cross-Origin-Opener-Policy: same-origin");
     header("Cross-Origin-Embedder-Policy: require-corp");
 // }
+echo cm("/admin");
 include(__DIR__ . "/assets/" . explode("/",$_SERVER["REQUEST_URI"])[2]);
