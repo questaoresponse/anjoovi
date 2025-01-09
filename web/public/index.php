2b4c341cb0bef39fbe46488e65f0e5c216aa420f
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-09 00:09:02 -0300

    "update"

--- web/public/index.php
+++ web/public/index.php
@@ -7,6 +7,10 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 } else if (cm("/noticia") || cm("/imagem") || cm("/musica") || cm("/texto") || cm("/video") || cm("/playlist") || cm("/product")){
     include(__DIR__ . '/../server/routes.php');
 } else {
+    if (cm("/admin") && (cm("/admin/noticias_cadastro") || cm("/admin/24_cadastro") || cm("/admin/imagens_cadastro") || cm("/admin/musicas_cadastro") || cm("/admin/textos_cadastro") || cm("/admin/videos_cadastro") || cm("/admin/products_cadastro"))){
+        header("Cross-Origin-Opener-Policy: same-origin");
+        header("Cross-Origin-Embedder-Policy: require-corp");
+    }
     include(__DIR__ . '/index.html');
 }
 ?>
