fe8714768ea4833f0ae699530be50255088ca827
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-08 22:22:33 -0300

    "update"

--- web/src/components/Global.tsx
+++ web/src/components/Global.tsx
@@ -73,7 +73,8 @@ class NavigateClass{
   }
 
   navigate(pathname:string,options:OptionsNavigate={changeURL:true,lookTop:true,callHandle:true}):void{
-    const urls=["/admin/noticias_cadastro","/admin/24_cadastro","/admin/imagens_cadastro","/admin/musicas_cadastro","/admin/textos_cadastro","/admin/playlists_cadastro","/admin/products_cadastro"];
+    const urls=["/admin/noticias_cadastro","/admin/noticias_edit","/admin/24_cadastro","/admin/imagens_cadastro","/admin/imagens_edit","/admin/musicas_cadastro","/admin/musicas_edit","/admin/textos_cadastro","/admin/textos_edit","/admin/playlists_cadastro","/admin/playlists_edit","/admin/products_cadastro","/admin/products_edit"];
+    console.log(this.pathname.split("/").slice(0,3).join("/"),pathname.split("/").slice(0,3).join("/"),urls.includes(this.pathname.split("/").slice(0,3).join("/")),!urls.includes(pathname.split("/").slice(0,3).join("/")));
     if ((this.pathname.startsWith("/admin") || pathname.startsWith("/admin")) && ((urls.includes(pathname.split("/").slice(0,3).join("/")) && !urls.includes(this.pathname.split("/").slice(0,3).join("/"))) || (urls.includes(this.pathname.split("/").slice(0,3).join("/")) && !urls.includes(pathname.split("/").slice(0,3).join("/"))))){
       window.location.href=pathname;
     }
