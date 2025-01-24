85973b6674a00dacf53e873840e92aca85ba5c59
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-24 15:01:07 -0300

    "update"

--- server/main2.php
+++ server/main2.php
@@ -3099,12 +3099,12 @@ Route::post("/verifyPeerId",function(){
     }
 });
 Route::post("/simple",function(){
-    $ffi = FFI::cdef(
-        "const char* saudacao();",   // Define a assinatura da função
-        "/teste.so"       // Caminho para a DLL (ou .so)
-    );
-    
-    // Chamar a função da DLL
-    echo $ffi->saudacao();
+    // $ffi = FFI::cdef(
+    //     "const char* saudacao();",   // Define a assinatura da função
+    //     "/teste.so"       // Caminho para a DLL (ou .so)
+    // );
+    // // Chamar a função da DLL
+    // echo $ffi->saudacao();
+    phpinfo();
 });
 include(__DIR__ . '/admin.php');
