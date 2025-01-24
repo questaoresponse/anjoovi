97f008ad1745c83cc55c2515226d4d3437326d52
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-24 16:05:51 -0300

    "update"

--- public_html/phpinfo.php
+++ public_html/phpinfo.php
@@ -1,11 +1,12 @@
 <?php
 try {
-    $ffi = FFI::cdef(
-        "const char* saudacao();",   // Define a assinatura da função
-        "/../server/teste.so"       // Caminho para a DLL (ou .so)
-    );
+    // $ffi = FFI::cdef(
+    //     "const char* saudacao();",   // Define a assinatura da função
+    //     "/../server/teste.so"       // Caminho para a DLL (ou .so)
+    // );
+    dl("/../server/teste.so");
     // Chamar a função da DLL
-    echo $ffi->saudacao();
+    echo saudacao();
 } catch (Exception $e){
     echo $e->getMessage();
 }
