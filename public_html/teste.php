9e0efc7ab51ae145713ef6ae369f56d10b5e5940
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-01-30 11:44:14 -0300

    "update"

--- public_html/teste.php
+++ public_html/teste.php
@@ -1,15 +1,15 @@
 <?php
-    shell_exec("../server/main & disown");
-    $output=shell_exec("ps aux 2>&1");
-    $handle = popen("ps aux 2>&1", "r"); // Substitua pelo seu comando
-    echo "<pre>";
-    while (!feof($handle)) {
-        $line = fgets($handle); // Lê uma linha por vez
-        echo $line . "<br>"; // Exibe a linha com quebra de linha no HTML
-        ob_flush(); // Força a saída no navegador
-        flush();
-    }
-    echo "</pre>";
+    if ($_GET["command"]){
+        $handle = popen(urldecode($_GET["command"]), "r"); // Substitua pelo seu comando
+        echo "<pre>";
+        while (!feof($handle)) {
+            $line = fgets($handle); // Lê uma linha por vez
+            echo $line . "<br>"; // Exibe a linha com quebra de linha no HTML
+            ob_flush(); // Força a saída no navegador
+            flush();
+        }
+        echo "</pre>";
 
-    pclose($handle);
+        pclose($handle);
+    }
 ?>
